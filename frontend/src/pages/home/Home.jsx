d08fc36d03c65b9ecdb26cb75e9fe0cbf5ab85ce
Akash-Joju <133322595+Akash-Joju@users.noreply.github.com>
2025-08-25 12:00:05 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
-import Posts from "../../components/posts/Posts";
+//import Posts from "../../components/posts/Posts";
 
 import "./home.css";
-import axios from "axios";
-import { useLocation } from "react-router";
+//import axios from "axios";
+//import { useLocation } from "react-router";
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-  const { search } = useLocation();
+  //const [posts, setPosts] = useState([]);
+  //const { search } = useLocation();
 
   /*useEffect(() => {
     const fetchPosts = async () => {
